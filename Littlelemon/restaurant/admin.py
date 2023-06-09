faf2f18b8702c8a36293d022161485bb7f54f147
Akhilesh <akhilesh.soni@somaiya.edu>
2023-06-09 12:28:19 +0530

    migrations done

--- Littlelemon/restaurant/admin.py
+++ Littlelemon/restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Booking_table,Menu_table
 # Register your models here.
+admin.site.register(Booking_table)
+admin.site.register(Menu_table)
--- Littlelemon/restaurant/models.py
+++ Littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking_table(models.Model):
+    name = models.CharField(max_length=200)
+    no_of_guests = models.IntegerField()
+    date = models.DateField()
+class Menu_table(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    inventory = models.IntegerField()
+
