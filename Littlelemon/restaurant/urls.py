76302d6656ab7331d2643c60cc28b1f35329de11
Akhilesh <akhilesh.soni@somaiya.edu>
2023-06-10 13:40:35 +0530

    template updated

--- Littlelemon/restaurant/urls.py
+++ Littlelemon/restaurant/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('',views.index,name='index')
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    # Add the remaining URL path configurations here
+    path('menu/',views.menu,name="menu"),
+    path('menu/<int:pk>/',views.display_menu_items,name="menu_item"),
 ]
--- Littlelemon/restaurant/views.py
+++ Littlelemon/restaurant/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from .models import Booking_table,Menu_table
+from .forms import BookingForm
 # Create your views here.
 
-def index(request):
-    return render(request,'index.html',{})
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+# Add your code here to create new views
+def menu(request):
+    menu_data = Menu_table.objects.all()
+    main_data = {'menu':menu_data}
+    return render(request,'menu.html',{"main":main_data})
+def display_menu_items(request,pk=None):
+    if pk is not None:
+        menu_item = Menu_table.objects.get(pk=pk)    
+    return render(request,'menu_item.html',{"menu_item":menu_item})    
